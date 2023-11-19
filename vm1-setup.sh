eb2c2a2e439edd6f3108aa4deb5289717563b929
Vladyslav Smolin <vladsmolin135@gmail.com>
2023-11-19 16:26:19 +0200

    Change in vm1-setup.sh file

--- vm1-setup.sh
+++ vm1-setup.sh
@@ -2,6 +2,8 @@
 # Оновити список пакетів
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+sudo apt-get install postgresql-client -y
 # Генерація SSH-ключа
 #ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 # Додавання публічного ключа до authorized_keys на vm2
